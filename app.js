2e974c47510ecb66bfd3beb537d1a6eb38479b7d
Niroshana Pathirana <niroshana.pathirana@outlook.com>
2017-12-17 14:25:05 +1100

    Removed console logs

--- app.js
+++ app.js
@@ -47,24 +47,17 @@ const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' +
 var recognizer = new builder.LuisRecognizer(LuisModelUrl);
 var intents = new builder.IntentDialog({ recognizers: [recognizer] })
 .matches('Greeting', (session) => {
-    console.log('Intent : \'%s\'.',session.intents);
-    session.send('Hi there, \%s\. I am Corian, Your Crown Connect Assistance. How can I help you today.', session.message.user.name);
+     session.send('Hi there, \%s\. I am Corian, Your Crown Connect Assistance. How can I help you today.', session.message.user.name);
 })
 .matches('CrownConnect', (session) => {
-    console.log('Intent : \'%s\'.',session.message.session.intents);
     session.send('Crown Connect is an ecosystem that provides capability to interconnect your organization with internal & external businesses, partners, customers and employees through various connection channels and business capabilities. It allows businesses to create and operate on new channels such as digital, social and mobile with secure, scalable and high performance.');
 })
 .matches('Help', (session) => {
-    console.log('Intent : \'%s\'.',session.message.session.intents);
-    session.send('You reached Help intent, you said \'%s\'.', session.message.text);
+   session.send('You reached Help intent, you said \'%s\'.', session.message.text);
 })
 .matches('Cancel', (session) => {
-    console.log('Intent : \'%s\'.',session.message.session.intents);
     session.send('You reached Cancel intent, you said \'%s\'.', session.message.IntentDialog);
 })
-/*
-.matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
-*/
 .onDefault((session) => {
     session.send('Sorry, I did not understand \'%s\'.', session.message.text);
 });
