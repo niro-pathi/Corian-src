1870d4f2add1e9b8da8c7c31f7a52a0d1ee81ab3
Niroshana Pathirana <niroshana.pathirana@outlook.com>
2017-12-17 11:26:41 +1100

    Added user name to greeting response

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' +
 var recognizer = new builder.LuisRecognizer(LuisModelUrl);
 var intents = new builder.IntentDialog({ recognizers: [recognizer] })
 .matches('Greeting', (session) => {
-    session.send('You reached Greeting intent, you said \'%s\'.', session.message.text);
+    session.send('Hi there \'%s\',', session.message.user.name);
 })
 .matches('Help', (session) => {
     session.send('You reached Help intent, you said \'%s\'.', session.message.text);
